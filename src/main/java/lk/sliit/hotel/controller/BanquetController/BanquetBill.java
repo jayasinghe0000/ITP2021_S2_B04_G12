fc18c418e5c311b4344e408183a72f792fa542da
IT20240738 <it20240738@my.sliit.lk>
2021-09-06 00:01:13 +0530

    banquet controller added

---
+++ src/main/java/lk/sliit/hotel/controller/BanquetController/BanquetBill.java
@@ -0,0 +1,37 @@
+package lk.sliit.hotel.controller.BanquetController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class BanquetBill {
+    @Autowired
+    IndexLoginBO indexLoginBO;
+    @Autowired
+    BanquetBO banquetBO;
+
+    @GetMapping("/banquetBill")
+    public ModelAndView loginPage(Model model){
+        ModelAndView mv = new ModelAndView("banquetBill");
+        model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
+
+        List<BanquetAddDTO> list = banquetBO.findBanquetBill();
+        mv.addObject("loadTable",list);
+
+        return mv;
+    }
+
+    @PostMapping("updateBill")
+    public String updateBill(@ModelAttribute BanquetAddDTO banquetAddDTO){
+        banquetBO.updateBill(banquetAddDTO);
+        return "redirect:/banquetBill";
+    }
+}
+
