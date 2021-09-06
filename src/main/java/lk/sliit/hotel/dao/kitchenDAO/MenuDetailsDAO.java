97d157b01b17aeb46de934565ab5298c3d0f1cec
Suchintha-Git <it20300814@my.sliit.lk>
2021-09-06 11:50:07 +0530

    kitchen dao,dto,entity file added

---
+++ src/main/java/lk/sliit/hotel/dao/kitchenDAO/MenuDetailsDAO.java
@@ -0,0 +1,18 @@
+package lk.sliit.hotel.dao.kitchenDAO;
+
+import lk.sliit.hotelManagement.entity.kitchen.FoodItem;
+import lk.sliit.hotelManagement.entity.kitchen.Menu;
+import lk.sliit.hotelManagement.entity.kitchen.MenuDetails;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuDetailsDAO extends CrudRepository<MenuDetails, Integer> {
+
+    void deleteMenuDetailsByFoodItemEquals(FoodItem id);
+
+    void deleteMenuDetailsByFoodItemAndMenuEquals(FoodItem foodItem, Menu menu);
+
+
+    Iterable<MenuDetails> findMenuDetailsByMenu_MenuId(int menuId);
+}
